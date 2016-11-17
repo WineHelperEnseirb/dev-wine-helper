18be23d69e0a1546bd36f99f45f44d9bab03133e
hitinui <hitinui.robert@enseirb-matmeca.fr>
2016-11-17 21:22:34 +0100

    Hello world added in fb_yomamabot

--- fb_yomamabot/views.py
+++ fb_yomamabot/views.py
@@ -11,16 +11,16 @@ from django.utils.decorators import method_decorator
 PAGE_ACCESS_TOKEN = "EAAYU6e7AspIBAOeiW55147fhZAgcchl3lJ45gb4xHlEvp8kdJyQoOb6ZCWGZCmEfhlaXom1bXuteol8XROjHtOLYdBJPZBbYO3GW1pqYkg8qb8ZCNWXHNkCTSbBR9oMIjz2Y6lmM3TNUcQesZAAZACQn741ZCAEHApuAHWG0OjnhFQZDZD"
 VERIFY_TOKEN = "verify_me"
 
-jokes = { 'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""", 
-                     """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""], 
-         'fat':      ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""", 
-                      """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """], 
-         'dumb': ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""", 
+jokes = { 'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
+                     """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
+         'fat':      ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""",
+                      """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """],
+         'dumb': ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""",
                   """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""] }
 
 # This function should be outside the BotsView class
-def post_facebook_message(fbid, recevied_message):           
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + PAGE_ACCESS_TOKEN 
+def post_facebook_message(fbid, recevied_message):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
@@ -33,7 +33,7 @@ class YoMamaBotView(generic.View):
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
-        
+
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return generic.View.dispatch(self, request, *args, **kwargs)
@@ -47,9 +47,13 @@ class YoMamaBotView(generic.View):
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
                 # Check to make sure the received call is a message call
-                # This might be delivery, optin, postback for other events 
+                # This might be delivery, optin, postback for other events
                 if 'message' in message:
                     # Print the message to the terminal
                     pprint(message)
                     post_facebook_message(message['sender']['id'], message['message']['text'])
-        return HttpResponse()    
+        return HttpResponse()
+
+class HelloView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Hello World!")
