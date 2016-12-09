c0eaa94579b8890fb5ac2e00508dbb0ff3649d8e
hitinui <hitinui.robert@enseirb-matmeca.fr>
2016-12-09 04:44:58 +0100

    Debug log added

--- wine_helper/wit_handler.py
+++ wine_helper/wit_handler.py
@@ -7,11 +7,12 @@ import json
 
 # Vendors
 from wit import Wit
+from pprint import pprint
 
 
 
-def wines():
-    return [{"appellation":"Pomerol","name":"Chateau Clinet","color":"red","vintage":"2011","price":"79","global_score":"95.14"},{"appellation":"Barsac","name":"Ch\u00e2teau Coutet","color":"white","vintage":"2009","price":"72.9","global_score":"95"},{"appellation":"Pomerol","name":"Petrus","color":"red","vintage":"2015","price":"87.53","global_score":"94.62"},{"appellation":"Languedoc","name":"T\u00eate de b\u00e9lier blanc","color":"white","vintage":"2014","price":"23.5","global_score":"94.2"},{"appellation":"Barsac","name":"Ch\u00e2teau Climens","color":"white","vintage":"2006","price":"78.9","global_score":"94.02"},{"appellation":"Haut-m\u00e9doc","name":"Ch\u00e2teau Bernadette","color":"red","vintage":"2012","price":"16.9","global_score":"93.67"},{"appellation":"Morgon","name":"Morgon","color":"red","vintage":"2015","price":"17","global_score":"93"},{"appellation":"Pomerol","name":"Ch\u00e2teau La Pointe","color":"red","vintage":"2011","price":"34.1","global_score":"90.74"},{"appellation":"Languedoc","name":"Prodige rose","color":"rose","vintage":"2015","price":"12.5","global_score":"90.5"},{"appellation":"C\u00f4tes de Provence","name":"MiP classic","color":"rose","vintage":"2015","price":"9.95","global_score":"90.17"},{"appellation":"Blaye","name":"Ch\u00e2teau La Roche Bazin","color":"red","vintage":"2014","price":"10.5","global_score":"90.15"},{"appellation":"Pomerol","name":"Carillon de Rouget","color":"red","vintage":"2011","price":"23.9","global_score":"89.82"},{"appellation":"Pays d'Oc","name":"Gris blanc","color":"rose","vintage":"2015","price":"8.1","global_score":"89.72"},{"appellation":"Graves","name":"Ch\u00e2teau d'Uza","color":"red","vintage":"2011","price":"12.9","global_score":"88.27"},{"appellation":"C\u00f4tes des Gascogne IGP","name":"Premi\u00e8res Grives","color":"white","vintage":"2015","price":"7.25","global_score":"87.1"},{"appellation":"C\u00f4tes des Gascogne IGP","name":"Colombard-Ugni blanc n\u00b03","color":"white","vintage":"2015","price":"5.5","global_score":"82.64"}]
+#def wines():
+#    return [{"appellation":"Pomerol","name":"Chateau Clinet","color":"red","vintage":"2011","price":"79","global_score":"95.14"},{"appellation":"Barsac","name":"Ch\u00e2teau Coutet","color":"white","vintage":"2009","price":"72.9","global_score":"95"},{"appellation":"Pomerol","name":"Petrus","color":"red","vintage":"2015","price":"87.53","global_score":"94.62"},{"appellation":"Languedoc","name":"T\u00eate de b\u00e9lier blanc","color":"white","vintage":"2014","price":"23.5","global_score":"94.2"},{"appellation":"Barsac","name":"Ch\u00e2teau Climens","color":"white","vintage":"2006","price":"78.9","global_score":"94.02"},{"appellation":"Haut-m\u00e9doc","name":"Ch\u00e2teau Bernadette","color":"red","vintage":"2012","price":"16.9","global_score":"93.67"},{"appellation":"Morgon","name":"Morgon","color":"red","vintage":"2015","price":"17","global_score":"93"},{"appellation":"Pomerol","name":"Ch\u00e2teau La Pointe","color":"red","vintage":"2011","price":"34.1","global_score":"90.74"},{"appellation":"Languedoc","name":"Prodige rose","color":"rose","vintage":"2015","price":"12.5","global_score":"90.5"},{"appellation":"C\u00f4tes de Provence","name":"MiP classic","color":"rose","vintage":"2015","price":"9.95","global_score":"90.17"},{"appellation":"Blaye","name":"Ch\u00e2teau La Roche Bazin","color":"red","vintage":"2014","price":"10.5","global_score":"90.15"},{"appellation":"Pomerol","name":"Carillon de Rouget","color":"red","vintage":"2011","price":"23.9","global_score":"89.82"},{"appellation":"Pays d'Oc","name":"Gris blanc","color":"rose","vintage":"2015","price":"8.1","global_score":"89.72"},{"appellation":"Graves","name":"Ch\u00e2teau d'Uza","color":"red","vintage":"2011","price":"12.9","global_score":"88.27"},{"appellation":"C\u00f4tes des Gascogne IGP","name":"Premi\u00e8res Grives","color":"white","vintage":"2015","price":"7.25","global_score":"87.1"},{"appellation":"C\u00f4tes des Gascogne IGP","name":"Colombard-Ugni blanc n\u00b03","color":"white","vintage":"2015","price":"5.5","global_score":"82.64"}]
 
 
 
@@ -130,6 +131,8 @@ client = Wit(access_token=os.getenv('WIT_TOKEN'), actions=actions)
 def treatment(request):
     session_id = 'my-user-session-42'
     context0 = {}
+    pprint("[DEBUG] wit request")
+    pprint(request)
     context1 = client.run_actions(session_id, request, context0)
     #client.interactive()
     return context1
