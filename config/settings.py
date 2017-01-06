b821d0bd083a4025a33f55060f7e76dd8c7971e1
hitinui <hitinui.robert@enseirb-matmeca.fr>
2017-01-06 10:44:28 +0100

    MONGODB config added in settings.py

--- config/settings.py
+++ config/settings.py
@@ -79,10 +79,20 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': '',
     }
 }
+
+_MONGODB_USER = 'winehelper.enseirb'
+_MONGODB_PASSWD = 'HwtB6c5a'
+_MONGODB_HOST = 'ds155718.mlab.com:55718'
+_MONGODB_NAME = 'wine-helper'
+_MONGODB_DATABASE_HOST = \
+    'mongodb://%s:%s@%s/%s' \
+    % (_MONGODB_USER, _MONGODB_PASSWD, _MONGODB_HOST, _MONGODB_NAME)
+
+mongoengine.connect(_MONGODB_NAME, host=_MONGODB_DATABASE_HOST)
+
 
 
 # Password validation
