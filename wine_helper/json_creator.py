6e075016f1d2753b61bc5965c914cf3c2396e139
AmbreCalas <calas.ambre@gmail.com>
2017-01-11 16:36:17 +0100

    peu importe

--- wine_helper/json_creator.py
+++ wine_helper/json_creator.py
@@ -31,6 +31,6 @@ def create_text_response(text):
 
 #function to create whatever option
 def create_whatever_button():
-    button_table = create_button_table('Peu importe')
+    button_table = create_button_table('')
     button_table['options'].append(create_button('Peu importe', 'peu importe'))
     return button_table
