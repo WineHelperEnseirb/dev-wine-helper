a492395c4825f5e101eb77096f6d24861c8728b1
William Estupina <william.estupina@gmail.com>
2017-01-13 15:59:33 +0100

    test

--- wine_helper/wit_handler.py
+++ wine_helper/wit_handler.py
@@ -163,6 +163,7 @@ def responsePriceApero(request):
     return askAdjustment(request)
 
 actions = {
+    'askStoryLine' : askStoryline,
     'reset' : reset,
     'sayGoodbye' : sayGoodbye,
     'responseApero' : responseApero,
