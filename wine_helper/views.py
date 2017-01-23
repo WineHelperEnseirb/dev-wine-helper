e4cce249cd0522e8be5541d12fbce6a24999ab00
FloPrm <flora.parmentier@gmail.com>
2017-01-23 15:49:56 +0100

    2e debug

--- wine_helper/views.py
+++ wine_helper/views.py
@@ -171,8 +171,7 @@ def _event_handler(event_type, slack_event):
 
     elif event_type == "message":
         sender_id = None
-        print "MESSAGE ICI \n \n"
-        print slack_event["event"]["text"]        
+        print "MESSAGE ICI \n \n"        
         message_id = slack_event["event"]["event_ts"]
         print message_id
         if "user" in slack_event["event"] and message_id != pyBot.last_message_id:
